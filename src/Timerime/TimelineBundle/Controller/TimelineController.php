bb3da3c9def3f89a71a39005931dd21eb55aa380
root <root@timerime2.(none)>
2011-03-26 15:40:34 +0000

    Added UserBundle

--- src/Timerime/TimelineBundle/Controller/TimelineController.php
+++ src/Timerime/TimelineBundle/Controller/TimelineController.php
@@ -5,7 +5,7 @@ namespace Timerime\TimelineBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TimelineController extends Controller
-{		
+{
 	/**
 	 * @return Doctrine\ORM\EntityManager
 	 */
@@ -13,7 +13,7 @@ class TimelineController extends Controller
 	{
 		return $this->get('doctrine.orm.entity_manager');
 	}
-	
+
 	/**
 	 * @return Symfony\Component\HttpFoundation\Request
 	 */
@@ -21,38 +21,37 @@ class TimelineController extends Controller
 	{
 		return $this->get('request');
 	}
-	
-    public function indexAction()
-    {    	
-    	$timelines = 		$this->getEntityManager()
-    							->getRepository('Timerime\TimelineBundle\Entity\Timeline')
-    							->getTimelines();
-    	    	
-        return $this->render('TimelineBundle:Default:index.html.php', array(
+
+	public function indexAction()
+	{
+		$timelines = 		$this->getEntityManager()
+								->getRepository('Timerime\TimelineBundle\Entity\Timeline')
+								->getTimelines();
+
+		return $this->render('TimelineBundle:Default:index.html.php', array(
         	'timelines'			=> $timelines,
-        ));
-    }
-    
-    public function viewTimelineAction()
-    {
-    	$timeline = 		$this->getEntityManager()
-    							->getRepository('Timerime\TimelineBundle\Entity\Timeline')
-    							->find($this->getRequest()->get('id'));
-    	
-        return $this->render('TimelineBundle:Default:view_timeline.html.php', array(
+		));
+	}
+
+	public function viewTimelineAction()
+	{
+		$timeline = 		$this->getEntityManager()
+								->getRepository('Timerime\TimelineBundle\Entity\Timeline')
+								->find($this->getRequest()->get('id'));
+		 
+		return $this->render('TimelineBundle:Default:view_timeline.html.php', array(
         	'timeline'			=> $timeline,
-        ));
-    }
-    
-    public function viewTimelineItemAction()
-    {    	
-    	$timelineItem = 		$this->getEntityManager()
-    							->getRepository('Timerime\TimelineBundle\Entity\TimelineItem')
-    							->find($this->getRequest()->get('id'));
-    	
-        return $this->render('TimelineBundle:Default:view_timeline_item.html.php', array(
+		));
+	}
+
+	public function viewTimelineItemAction()
+	{
+		$timelineItem = 		$this->getEntityManager()
+									->getRepository('Timerime\TimelineBundle\Entity\TimelineItem')
+									->find($this->getRequest()->get('id'));
+		 
+		return $this->render('TimelineBundle:Default:view_timeline_item.html.php', array(
         	'timelineItem'		=> $timelineItem,
-        ));
-    }
+		));
+	}
 }
- 
