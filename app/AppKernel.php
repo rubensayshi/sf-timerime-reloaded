dcdcb36a6440e7792746e43a273dda434af56836
root <root@timerime2.(none)>
2011-03-27 20:36:29 +0000

    Added docs and enabled login

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\ZendBundle\ZendBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
 
             // register your bundles
             //new Sensio\HelloBundle\HelloBundle(),
